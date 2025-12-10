6b8255825ffb7efc2031ee6c7f12902a8dcd8b62
wahyuSap10 <wahyuiman1004@gmail.com>
2025-12-10 23:14:02 +0700

    Add files via upload

---
+++ love-letter-2.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  var envelope = $("#envelope");
+  var btn_open = $("#open");
+  var btn_reset = $("#reset");
+
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
+
+  function open() {
+    envelope.addClass("open").removeClass("close");
+  }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+  }
+});
+
+// tombol gift
+const gift = document.getElementById("gift");
+
+if (gift) {
+  gift.addEventListener("click", function () {
+    window.location.href = "flower.html"; 
+  });
+}
